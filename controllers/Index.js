711be039876ac5161a69c40410a3de7cbc8aa32f
Rstar-1 <rajshetye.5855@gmail.com>
2024-05-27 20:40:35 +0530

    fvd

--- controllers/Index.js
+++ controllers/Index.js
@@ -1,36 +1,9 @@
-const AuthLoginController = require('./bloglogincontroller/BlogLoginController.js');
 const BlogController = require('../controllers/blogcontroller/BlogController.js');
-const BlogCategoryController = require('../controllers/blogcontroller/BlogCategoryController.js');
-const PlansController = require('../controllers/plancontroller/PlanController.js');
-const PlansControllertwo = require('../controllers/plancontroller/PlanController2.js');
-const PlansControllerthird = require('../controllers/plancontroller/PlanController3.js');
 const SeoController = require('../controllers/seocontroller/SeoController.js');
-const TextController = require('../controllers/cmscontroller/TextController.js');
-const ImageController = require('../controllers/cmscontroller/ImageController.js');
-const BasicController = require('../controllers/plancontroller/BasicController.js');
-const PremiumController = require('../controllers/plancontroller/PremiumController.js');
-const SuperPremiumController = require('../controllers/plancontroller/SuperPremiumController.js');
-const ProjectController = require('../controllers/projectcontroller/ProjectController.js');
-const BrandController = require('../controllers/brandcontroller/BrandController.js');
-const ReviewController = require("../controllers/reviewcontroller/ReviewController.js");
 const webauthController = require("../controllers/webauthcontroller/Webauthcontroller.js");
 
 module.exports = {
-  AuthLoginController,
   BlogController,
-  BlogCategoryController,
-  PlansController,
-  PlansControllertwo,
-  PlansControllerthird,
   SeoController,
-  TextController,
-  TextController,
-  ImageController,
-  BasicController,
-  PremiumController,
-  SuperPremiumController,
-  ProjectController,
-  BrandController,
-  ReviewController,
   webauthController,
 };
